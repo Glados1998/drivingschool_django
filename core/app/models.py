719aca98fd3175cde95061c22e14e7038d8c0386
Glados1998 <j.greder@staro.ch>
2023-04-03 23:08:37 +0200

    minor changes added

--- core/app/models.py
+++ core/app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Courses(models.Model):
-    course_hours = models.IntegerField()
+    course_hours = models.IntegerField(null=True)
     course_date = models.DateField()
     course_duration = models.DurationField()
     course_place = models.CharField(max_length=50)
@@ -20,6 +20,8 @@ class Student(models.Model):
     password = models.CharField(max_length=30)
     email = models.EmailField()
     courses = models.ManyToManyField(Courses)
+    hours_of_taken_courses = models.IntegerField(null=True)
+    hours_of_paid_courses = models.IntegerField(null=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
