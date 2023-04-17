e976924f2c699ccef78a67b44a06366d1041e83f
Glados1998 <j.greder@staro.ch>
2023-04-17 09:02:16 +0200

    feat/ crud user complete, need further modifications

--- driving_school/app/forms.py
+++ driving_school/app/forms.py
@@ -35,8 +35,46 @@ class StudentCreationForm(forms.ModelForm):
             'email': forms.EmailInput(),
             'name': forms.TextInput(),
             'role': forms.Select(attrs={'class': 'form-control'}),
-            'is_active': forms.Select(attrs={'class': 'form-control'}),
+            'is_active': forms.CheckboxInput(),
             'assigned_courses': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        },
+        labels = {
+            'password': 'Password',
+            'email': 'Email',
+            'name': 'Name',
+            'role': 'Role',
+            'is_active': 'Active',
+            'assigned_courses': 'Assigned Courses',
+        }
+        help_texts = {
+            'password': 'Password must be at least 8 characters long',
+            'email': 'Please enter a valid email address',
+            'name': 'Please enter a valid name',
+            'role': 'Please select a valid role',
+            'is_active': 'Please select if the user is active',
+            'assigned_courses': 'Please select at least one course',
+        },
+        error_messages = {
+            'password': {
+                'min_length': 'Password must be at least 8 characters long',
+            },
+            'email': {
+
+                'invalid': 'Please enter a valid email address',
+            },
+            'name': {
+                'invalid': 'Please enter a valid name',
+            },
+            'role': {
+                'invalid': 'Please select a valid role',
+            },
+            'is_active': {
+
+                'invalid': 'Please select if the user is active',
+            },
+            'assigned_courses': {
+                'invalid': 'Please select at least one course',
+            },
         }
 
 
@@ -44,11 +82,47 @@ class StudentEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = {'password', 'email', 'name', 'role', 'assigned_courses'}
-        exclude = {'is_active'}
         widgets = {
             'password': forms.PasswordInput(),
             'email': forms.EmailInput(),
             'name': forms.TextInput(),
             'role': forms.Select(attrs={'class': 'form-control'}),
+            'is_active': forms.CheckboxInput(),
             'assigned_courses': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'password': 'Password',
+            'email': 'Email',
+            'name': 'Name',
+            'role': 'Role',
+            'is_active': 'Active',
+            'assigned_courses': 'Assigned Courses',
+        }
+        help_texts = {
+            'password': 'Password must be at least 8 characters long',
+            'email': 'Please enter a valid email address',
+            'name': 'Please enter a valid name',
+            'role': 'Please select a valid role',
+            'is_active': 'Please select if the user is active',
+            'assigned_courses': 'Please select at least one course',
+        }
+        error_messages = {
+            'password': {
+                'min_length': 'Password must be at least 8 characters long',
+            },
+            'email': {
+                'invalid': 'Please enter a valid email address',
+            },
+            'name': {
+                'invalid': 'Please enter a valid name',
+            },
+            'role': {
+                'invalid': 'Please select a valid role',
+            },
+            'is_active': {
+                'invalid': 'Please select if the user is active',
+            },
+            'assigned_courses': {
+                'invalid': 'Please select at least one course',
+            },
         }
